a7daa32ec4c0623a70245d79859e1102bfd74584
Andromeda <geraskin19970309@gmail.com>
2023-10-10 12:26:00 +0300

    add basic layout and colors

--- app/src/main/java/ua/com/andromeda/artspace/MainActivity.kt
+++ app/src/main/java/ua/com/andromeda/artspace/MainActivity.kt
@@ -3,13 +3,29 @@ package ua.com.andromeda.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ua.com.andromeda.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtWorkLayout()
                 }
             }
         }
@@ -30,17 +46,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtWorkLayout() {
+    Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
+        ArtWorkImage(modifier = Modifier.padding(horizontal = 8.dp))
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Artwork title",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.Center)
+            )
+            Text(
+                text = "Author (2023)",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.Center)
+            )
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentWidth()
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth(.5f)
+                        .height(50.dp)
+                ) {
+                    Text(text = "⬅️ Previous", fontSize = 22.sp)
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text(text = "Next ➡️", fontSize = 22.sp)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtWorkLayout()
     }
+}
+
+@Composable
+fun ArtWorkImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.art_photo_1),
+        contentDescription = "Some description",
+        modifier = modifier
+    )
 }
